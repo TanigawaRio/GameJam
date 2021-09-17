ffdc12c52eff3b24c30693b5e4831848fcf09ff1
TanigawaRio <74332390+TanigawaRio@users.noreply.github.com>
2021-09-17 11:51:33 +0900

    Add Title

---
+++ GameJam_Project/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string NextScene)
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+}
